58f2cbfe871e03781863ee37561f2b1410fac876
syuulee <yuyu00lee@gmail.com>
2022-10-24 16:27:29 +0900

    2022.10.24

--- week_11/js/main.js
+++ week_11/js/main.js
@@ -1,10 +1,5 @@
 $(function () {
 
-    $('.more').on('click', function () {
-        $(this).toggleClass('on');
-        $('.detail_infor').toggleClass('on');
-    });
-
     $('.tab_menu>li').on('click', function (e) {
         e.preventDefault();
         // console.log($(this).parent().index());
